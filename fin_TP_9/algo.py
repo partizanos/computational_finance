d72afb6509894384c94658875a2270fc65bd55d9
partizanos <dimi.proios@gmail.com>
2019-05-12 00:26:25 +0200

    fin_TP_9/.idea/fin_TP_9.iml fin_TP_9/.idea/misc.xml fin_TP_9/.idea/modules.xml fin_TP_9/.idea/other.xml fin_TP_9/.idea/vcs.xml fin_TP_9/algo.py fin_TP_9/report.ipynb

---
+++ fin_TP_9/algo.py
@@ -0,0 +1,17 @@
+import math
+import numpy as np
+import scipy.stats as si
+import sympy as sy
+import sympy.stats as systats
+
+def Black_Scholes_Algo(S, K, T, r, sigma):
+    #S: spot price
+    #K: strike price
+    #T: time to maturity
+    #r: interest rate
+    #sigma: volatility of underlying asset
+    d1 = (np.log(S / K) + (r + 0.5 * sigma ** 2) * T) / (sigma * np.sqrt(T))
+    d2 = (np.log(S / K) + (r - 0.5 * sigma ** 2) * T) / (sigma * np.sqrt(T))
+    call = (S * si.norm.cdf(d1, 0.0, 1.0) - K * np.exp(-r * T) * si.norm.cdf(d2, 0.0, 1.0))
+    
+    return call
